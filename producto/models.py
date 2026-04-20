323e0ad33107149b364480a5562f5aba1fd15f1d
Rodolfo <rodolfogroero2@gmail.com>
2026-04-20 16:20:06 -0300

    Agregar modelo Producto: definir campos nombre, precio y relación con Cuenta.

--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from core.models import BaseModel
 # Create your models here.
+class Producto(BaseModel):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cuenta = models.ForeignKey('cuenta.Cuenta', on_delete=models.CASCADE, related_name='productos')
+
+    class Meta:
+        db_table = 'producto'
