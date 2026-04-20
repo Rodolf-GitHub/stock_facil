26562dc58f81bf79fb39623a7eff2c9ee666f017
Rodolfo <rodolfogroero2@gmail.com>
2026-04-20 15:39:23 -0300

    Agregar esquemas de usuario para creación y actualización

--- usuario/schemas.py
+++ usuario/schemas.py
@@ -1,2 +1,18 @@
 from ninja import Schema, ModelSchema
 from typing import Optional
+
+class UsuarioSchema(ModelSchema):
+    class Meta:
+        model = 'usuario.Usuario'
+        exclude = ['hash_password', 'token']
+
+class UsuarioCreateSchema(Schema):
+    cuenta_id: int
+    email: str
+    password: str
+
+class UsuarioUpdateSchema(Schema):
+    email: Optional[str] = None
+    password: Optional[str] = None
+
+
